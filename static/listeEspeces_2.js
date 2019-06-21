189a595121b4fd737ff98222e3bc74b026b58fbd
Jean-Baptiste Desbas <jb.desbas@gmail.com>
2019-06-21 12:15:30 +0200

    default order by protection statut

--- static/listeEspeces_2.js
+++ static/listeEspeces_2.js
@@ -8,6 +8,7 @@ function myDataTable(domid){
           bPaginate: false,
           "bAutoWidth": false,
           language:{ search:         "Filtrer :"},
+          order:[[0,"desc"],[ 4, 'asc' ]],
           columnDefs: [
                 { width: "35%" , targets: [3, 4]}
 
