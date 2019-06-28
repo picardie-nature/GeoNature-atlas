9392a5305c33932564dd2dde215a2948a019e46d
Jean-Baptiste Desbas <jb.desbas@gmail.com>
2019-06-28 14:42:40 +0200

    refresh vm script

--- scripts/refresh_vm.sh
+++ scripts/refresh_vm.sh
@@ -7,11 +7,15 @@ db=geonature2db
 
 #PGPASSWORD=$1 nohup psql -U $user -h $host -f import_foreign.sql -d $db
 
-PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW atlas.vm_observations"
-PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW atlas.vm_observations_mailles"
-PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW atlas.vm_taxons"
-PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW atlas.vm_taxons_plus_observes"
-PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW atlas.vm_search_taxon"
-PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW atlas.vm_mois"
-PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW atlas.vm_medias"
+PGPASSWORD=$1 psql -h $host -w -U $user $db -f "/home/jb/GeoNature-atlas/scripts/clicnat2geonature-atlas.sql"
 
+PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW CONCURRENTLY atlas.vm_observations"
+PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW CONCURRENTLY atlas.vm_observations_mailles"
+PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW CONCURRENTLY atlas.vm_taxons"
+PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW CONCURRENTLY atlas.vm_taxons_plus_observes"
+PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW CONCURRENTLY atlas.vm_search_taxon"
+PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW atlas.vm_mois"
+PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW CONCURRENTLY atlas.vm_medias"
+PGPASSWORD=$1 psql -h $host -w -U $user $db -c "REFRESH MATERIALIZED VIEW CONCURRENTLY atlas.vm_atlas_thematique"
+
+echo $(date)
