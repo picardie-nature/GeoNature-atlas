98c9ff79398bbdd91cdec8115b0af6f9d866213c
JB <jb.desbas@gmail.com>
2020-03-17 11:26:57 +0100

    color chart

--- static/chart_territory.js
+++ static/chart_territory.js
@@ -1,6 +1,4 @@
 var drawChart = function(elementId, labels, data, title){ 
-
-
     var ctx = document.getElementById(elementId).getContext('2d');
     var chart = new Chart(ctx, {
         // The type of chart we want to create
@@ -11,7 +9,7 @@ var drawChart = function(elementId, labels, data, title){
             labels: labels,
             datasets: [{
                 label: 'Nombre d\'espèces',
-                backgroundColor: ["#4cd195", "#f1f58c","#92baed","#adf7b8","#f18e8e"],
+                backgroundColor: ["#4cd195", "#f1f58c","#92baed","#adf7b8","#f18e8e","#ffbb00","#cc0066","#cccc00"],
                 borderColor: 'rgb(255, 255, 255)',
                 data: data
             }]
