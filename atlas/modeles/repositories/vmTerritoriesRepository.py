3f040597b6cdbe27966572cc6f4f99ba1cd5cbc7
JB <jb.desbas@gmail.com>
2020-03-19 11:09:01 +0100

    search territory

--- atlas/modeles/repositories/vmTerritoriesRepository.py
+++ atlas/modeles/repositories/vmTerritoriesRepository.py
@@ -15,3 +15,13 @@ def getTerritorieKnoweldgeEvolution(connection, area_code):
         JOIN atlas.vm_territories t ON t.id_area =tke.id_area WHERE area_code=:thisAreaCode;"""
     req = connection.execute(text(sql), thisAreaCode=area_code)
     return [ dict(r) for r in req  ]
+
+
+def getTerritoriesSearch(connection, search, limit=50):
+    sql = "SELECT area_name, area_code, area_type  FROM atlas.vm_territories WHERE area_name ILIKE :thisSearch ORDER BY char_length(area_name) LIMIT :thisLimit"
+    req = connection.execute(text(sql), thisSearch='%{}%'.format(search), thisLimit=limit)
+    TerritoriesList = list()
+    for r in req:
+        temp = {'label': r[0], 'value': r[1], 'type':r[2]}
+        TerritoriesList.append(temp)
+    return TerritoriesList
