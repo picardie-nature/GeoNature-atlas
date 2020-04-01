70bc988f56b340065693c91948abf7569f18d69f
JB <jb.desbas@gmail.com>
2020-04-01 09:57:23 +0200

    fix order knowledge evolution

--- atlas/modeles/repositories/vmTerritoriesRepository.py
+++ atlas/modeles/repositories/vmTerritoriesRepository.py
@@ -12,7 +12,7 @@ def getTerritorieFromCode(connection, area_code):
 def getTerritorieKnoweldgeEvolution(connection, area_code):
     sql="""SELECT n_taxon, n_occurence, n_observer, date_part('year', t) AS "year"
         FROM atlas.vm_territories_knoweldge_evolution tke
-        JOIN atlas.vm_territories t ON t.id_area =tke.id_area WHERE area_code=:thisAreaCode;"""
+        JOIN atlas.vm_territories t ON t.id_area =tke.id_area WHERE area_code=:thisAreaCode ORDER BY t;"""
     req = connection.execute(text(sql), thisAreaCode=area_code)
     return [ dict(r) for r in req  ]
 
